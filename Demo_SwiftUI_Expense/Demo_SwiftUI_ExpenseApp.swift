d252df961e19c4bd61a68fe082c939924b4f3804
Tak Liu <tliu@connected.io>
2022-07-04 11:11:08 -0400

    first commit

---
+++ Demo_SwiftUI_Expense/Demo_SwiftUI_ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUI_ExpenseApp.swift
+//  Demo_SwiftUI_Expense
+//
+//  Created by Tak Liu on 2022-07-04.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUI_ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
